d3d97bc8f8b9a08708c2a9f468d103b5dcc2f62a
kho903 <gmldnr2222@naver.com>
2019-09-06 17:33:38 +0900

    2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324223,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"Jihun",
+            email:"a@naver.com"
+        }
+    },
+    {
+        id:1212,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"Jihun",
+            email:"a@naver.com"
+        }
+    },
+    {
+        id:234234,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"Jihun",
+            email:"a@naver.com"
+        }
+    },
+    {
+        id:23423423,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"Jihun",
+            email:"a@naver.com"
+        }
+    }
+]
